def08fa6eb26b20d70986f4d2f13c8f93ab78264
romerodevelopment <64169519+romerodevelopment@users.noreply.github.com>
2021-11-03 01:06:20 -0300

    politicas cors - credentials cookies{

--- cors/server/server.js
+++ cors/server/server.js
@@ -8,16 +8,19 @@ app.use(cookieParser())
 app.use(morgan('tiny'));
 
 let f = (req, res) => {
-  res.header("Access-Control-Allow-Origin","*"); //Tambien es posible indicar el dominio,el * indica que cualquier origen puede acceder al recurso
+  res.header("Access-Control-Allow-Origin","http://localhost:3030"); //Tambien es posible indicar el dominio,el * indica que cualquier origen puede acceder al recurso
+  res.header("Access-Control-Allow-Credentials", "true");
+  console.log(req.cookies);
   res.send({
     hola: "mundo"
   });
 }
 
 app.options("/",(req,res)=>{
-  res.header("Access-Control-Allow-Origin","*");
+  res.header("Access-Control-Allow-Origin","http://localhost:3030");
   res.header("Access-Control-Allow-Methods","PUT"); //Se habilita el metodo PUT para habilitar la peticion preflight
   res.header("Access-Control-Allow-Headers", "Content-Type, Authentication"); //Se habilitan los encabezados content type y authentication para la paeticion preflight
+  res.header("Access-Control-Allow-Credentials", "true"); //Permite que un origen distinto envie cookies
   res.status(204).send()
 })
 
